e60a489a9173a50723720c47a941b303ed722698
joelmaxwellr <joelmaxwellr@gmail.com>
2022-08-16 22:26:25 -0400

    update

--- routes/routers.js
+++ routes/routers.js
@@ -20,20 +20,46 @@ router.get("/create",(req, res)=>{
  
 
 router.get("/edit/:id", (req,res)=>{
-   // const id = req.params.id
-   /*  conection.query(" SELECT * FROM users WHERE id=?",[id],(error, results)=>{
+  // const id = req.params.id 
+    //res.render("edit",{id:1})
+    const id = req.params.id
+    conection.query(" SELECT * FROM users WHERE id=?",[id],(error, results)=>{
         if (error) {
             throw error;
         }else{
             res.render("edit",{user:results[0]})
         }
-    }) */
+    })
 
 })
 
 router.post("/save",(req,res)=>{
     const {nombre,precio} = req.body
    conection.query("INSERT INTO users SET ?",{nombre:nombre, precio:precio},(error,results)=>{
+    if (error) {
+        console.log(error)
+    } else {
+        res.redirect("/")
+    }
+   })
+})
+
+router.post("/update",(req,res)=>{
+    const {id, nombre,precio} = req.body
+    console.log(req.body)
+   conection.query("UPDATE users SET ? WHERE id=?",[{nombre: nombre, precio:precio} , id],(error,results)=>{
+    if (error) {
+        console.log(error)
+    } else {
+        res.redirect("/")
+    }
+   })
+})
+
+router.post("/delete/:id",(req,res)=>{
+    const {id, nombre,precio} = req.body
+    console.log(req.body)
+   conection.query("DELETE users SET ? WHERE id=?",[id],(error,results)=>{
     if (error) {
         console.log(error)
     } else {
